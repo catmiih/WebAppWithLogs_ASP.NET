5df8b1cce2acd354f9055f8765507960ac4d2015
catmiih <emilyleme.dev@gmail.com>
2023-03-08 12:18:29 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Data;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private ApplicationDbContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Usuario acessou a página Index");
+            var usuarios = _context.Users.ToList();
+
+            Console.WriteLine(usuarios[0].NormalizedUserName);
+
+            _logger.LogWarning("Cuidado!");
+        }
+    }
+}
